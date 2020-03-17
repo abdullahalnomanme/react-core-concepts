f4f5f375a5226da2a46d870dd37915c89a011de8
Abdullah Al Noman <abdullahalnomanme@gmail.com>
2020-03-17 00:56:21 -0700

    last commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+const styleCss ={
+    color:'red',
+    backgroundColor:'gray',
+    border:'5px solid red',
+    padding:'10px',
+    borderRadius:'10px',
+}
 function App() {
     let person = {
         name: 'Dr. Mahfujur Rahman',
@@ -11,13 +17,7 @@ function App() {
         name: 'Eva Rahman',
         job: 'Kokil konti'
       } 
-      var style ={
-          color:'red',
-          backgroundColor:'gray',
-          border:'5px solid red',
-          padding:'10px',
-          borderRadius:'10px',
-      }
+const nayoks = ['Anowar', 'japor', 'razzak', 'salman']
     return (
     <div className = "App">
         <header className = "App-header">
@@ -27,13 +27,17 @@ function App() {
             <p>Edit 22 < code > src / App.js </code>and save to reload. </p >
             <p>My First Paragrap </p> 
 
-            <h2 className="" style ={style}>
+            <h2 className="" style ={styleCss}>
                 Name: {person.name +" is a "+ person.job}
             </h2>
 
             <h4 style = {{backgroundColor:'blue', border:'5px solid green', padding:'10px', }}>
                 Singer: {person2.name +" is a "+ person.job}
             </h4>
+            <Person name= {nayoks[0]} nayka="Mushumi"></Person>
+            <Person name="Rubel hossain" nayka="Purnima"></Person>
+            <Person name="Ellias hossain" nayka="shabnur"></Person>
+            <Person name="salman sah" nayka="opu bisswash" ami="ami noman"></Person>
         </header>
         
     </div>
@@ -41,3 +45,17 @@ function App() {
 }
 
 export default App;
+
+// Component
+let style2 = {
+    border: "2px solid red", boxShadow:"0px 0px 5px 5px gray", margin:"20px", padding: "20px"
+}
+function Person(props){
+    console.log(props);
+    return (
+        <div style = {style2}>
+            <h1>Name: {props.name } </h1>
+    <h3 style = {{color:'gold'}}>Hero of {props.nayka} </h3>
+        </div>
+    )
+}
